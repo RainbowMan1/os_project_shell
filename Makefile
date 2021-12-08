0ab4c34d32adb12b11106a96f2562eb2e823b3c2
Nikesh Subedi <nikesh.subedi@bison.howard.edu>
2021-12-08 20:33:53 +0000

    initial

---
+++ Makefile
@@ -0,0 +1,4 @@
+shell: shell.c
+	gcc shell.c -o shell
+debug_shell: shell.c
+	gcc -g shell.c -o shell
---
+++ a.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+
+int main(){
+	while (1){
+		sleep(5);
+		printf("Awake and now sleep again\n");
+	}
+}
